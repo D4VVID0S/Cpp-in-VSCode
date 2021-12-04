81c7059da98dcfc5927ec569a580fb3ee5c4cef5
D4VVID0S <matix09@live.it>
2021-12-04 21:17:01 +0100

    Init

---
+++ 003 - Kurs Udemy/dynamAlokacPamieci.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //dynamiczne alokowanie pamieci w tablicach
+
+    int n = 0;
+    cout << "Podaj rozmiar tablicy: ";
+    cin >> n;
+
+    int *wsk = new int[n]; //new int tworzy zmienna ktora bedzie nam alokowac pamiec w pozniejszym momencie, nie na start programu
+
+    for(int i = 0; i < n; i++)
+        cout << wsk[i] << endl;
+
+    delete [] wsk;
+
+    if (wsk != NULL)
+        cout << "wsk = " << wsk << endl;
+    else
+        cout << "wsk = NULL" << endl;
+
+    return 0;
+}
